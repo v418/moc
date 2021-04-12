824f645bb50570cdb70a6b84e59cf03b8def4ba4
vishnu <vishnu.madduru418@gmail.com>
2021-04-12 11:48:48 +0530

    commit and push

---
+++ 12345/src/test/java/new12/testcase1.java
@@ -0,0 +1,28 @@
+package new12;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testcase1 {
+
+	@Test
+	public void test1() {
+	System.out.println("vishu1");
+	}
+	@Test
+	public void test2() {
+	System.out.println("vishu2");
+	}
+	
+	@Test
+	public void test3() {
+	System.out.println("vishu3");
+	}
+	@Test
+	public void test4() {
+	System.out.println("vishu4");
+	}
+}
+
+
